475ea129f9a12f0f2a98441915bf642e65e46c8d
kilisio <pkilisio@gmail.com>
2019-12-26 22:53:31 +0300

    refactor(func_extend.js): converted to scoped variables

--- src/func_extend.js
+++ src/func_extend.js
@@ -1,6 +1,6 @@
 export default function() {
 	var process = function(destination, source) {	
-	    for(var key in source) {
+	    for(let key in source) {
 			if(Object.prototype.hasOwnProperty.call(source, key)) {
 			    destination[key] = source[key];
 			}
@@ -8,7 +8,7 @@ export default function() {
 	    return destination;
 	};
 	var result = arguments[0];
-	for(var i=1; i<arguments.length; i++) {
+	for(let i=1; i<arguments.length; i++) {
 		result = process(result, arguments[i]);
 	}
 	return result;
