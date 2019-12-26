21d2839806e718699ad9b6e66239b50d4fa8ac68
kilisio <pkilisio@gmail.com>
2019-12-26 22:47:19 +0300

    fix(func_analyzer.js): fixed semantic syntax errors

--- src/func_analyzer.js
+++ src/func_analyzer.js
@@ -16,11 +16,12 @@ export default function(prop) {
 		};
 	}
 
-	for(var i=0; i<prop.length, c=prop[i]; i++) {
+	for(let i=0; i<prop.length; i++) {
+        let c=prop[i];
 		if(c === "[" && !readingAttributes) {
 			readingAttributes = true;
 		} else if(readingAttributes) {
-			if(c != "]") {
+			if(c !== "]") {
 				attributes += c;
 			} else {
 				readingAttributes = false;
@@ -29,7 +30,7 @@ export default function(prop) {
 		} else if(c === "." && !readingClass) {
 			readingClass = true;
 		} else if(readingClass) {
-			if(c != "." && c != "#" && c != "[" && c != "]") {
+			if(c !== "." && c !== "#" && c !== "[" && c !== "]") {
 				className += c;
 			} else {
 				classes.push(className);
@@ -40,38 +41,39 @@ export default function(prop) {
 		} else if(c === "#" && !readingId) {
 			readingId = true;
 		} else if(readingId) {
-			if(c != "." && c != "#" && c != "[" && c != "]") {
+			if(c !== "." && c !== "#" && c !== "[" && c !== "]") {
 				idName += c;
 			} else {
 				readingId = false;
 				i -= 1;
 			}
-		} else if(c != "." && c != "#" && c != "[" && c != "]") {
+		} else if(c !== "." && c !== "#" && c !== "[" && c !== "]") {
 			res.tag += c;
 		}
 	}
 
 	// if ends with a class
-	if(className != "") classes.push(className);
+	if(className !== "") { classes.push(className); }
 
 	// collecting classes
 	var clsStr = '';
-	for(var i=0; cls=classes[i]; i++) {
+	for(let i=0; i<classes.length; i++) {
+        let cls=classes[i];
 		clsStr += clsStr === "" ? cls : " " + cls;
 	}
-	res.attrs += clsStr != "" ? 'class="' + clsStr + '"' : '';
+	res.attrs += clsStr !== "" ? 'class="' + clsStr + '"' : '';
 
 	// if ends on id
-	if(idName != "") {
-		res.attrs += (res.attrs != "" ? " " : "") + 'id="' + idName + '"';
+	if(idName !== "") {
+		res.attrs += (res.attrs !== "" ? " " : "") + 'id="' + idName + '"';
 	}
 
 	// if div tag name is skipped
-	if(res.tag === "" && res.attrs != "") res.tag = "div";
+	if(res.tag === "" && res.attrs !== "") { res.tag = "div"; }
 
 	// collecting attributes
-	if(attributes != "") {
-		res.attrs += (res.attrs != "" ? " " : "") + attributes;
+	if(attributes !== "") {
+		res.attrs += (res.attrs !== "" ? " " : "") + attributes;
 	}
 
 	return res;
