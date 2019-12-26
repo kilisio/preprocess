b8e7a6cf3e392a93f3f0613c2a1f572bf7dffe18
kilisio <pkilisio@gmail.com>
2019-12-26 22:52:23 +0300

    fix(func_css.js): fixed semantic syntax errors

--- src/func_css.js
+++ src/func_css.js
@@ -1,5 +1,4 @@
 import transformUppercase from "./func_transform.js";
-import extend from "./func_extend.js";
 import temp_eng from "./func_template.js";
 
 var newline = '\n',
@@ -12,7 +11,7 @@ var newline = '\n',
 var toCSS = function(rules, options, indent) {
 	var css = '';
 	indent = indent || ['', '  '];
-	for(var selector in rules) {
+	for(let selector in rules) {
 		// handling raw content
 		if(selector.indexOf("____raw") === 0) {
 			css += rules[selector][selector] + newline;
@@ -20,7 +19,7 @@ var toCSS = function(rules, options, indent) {
 		} else {
 			var entityStyle = indent[0] + selector.replace(/~~(.+)~~/, '').replace(/^%.*?%/, '') + ' {' + newline;
 			var entity = '';
-			for(var prop in rules[selector]) {
+			for(let prop in rules[selector]) {
 				var value = rules[selector][prop];
 				if(value === "") {
 					value = '""';
@@ -32,7 +31,7 @@ var toCSS = function(rules, options, indent) {
 					entity += indent[1] + transformUppercase(prop) + ': ' + value + ';' + newline;
 				}
 			}
-			if(entity != '') {
+			if(entity !== '') {
 				entityStyle += entity;
 				entityStyle += indent[0] + '}' + newline;
 				css += entityStyle;
@@ -40,44 +39,44 @@ var toCSS = function(rules, options, indent) {
 		}
 	}
 	return css;
-}
+};
 
-var toJSON = function(rules, options) {
+var toJSON = function(rules) {
 	var result = {};
-	for(var stylesheet in rules) {
+	for(let stylesheet in rules) {
 		var styles = rules[stylesheet];
-		if(stylesheet == 'mainstream') {
-			for(var selector in styles) {
-				result[selector] = {}
-				for(var prop in styles[selector]) {
+		if(stylesheet === 'mainstream') {
+			for(let selector in styles) {
+				result[selector] = {};
+				for(let prop in styles[selector]) {
 					result[selector][prop] = styles[selector][prop];
 				}
 			}			
 		} else if(stylesheet.indexOf('@media') >= 0) {
 			result[stylesheet] = {};
-			for(var selector in styles) {
-				result[stylesheet][selector] = {}
-				for(var prop in styles[selector]) {
+			for(let selector in styles) {
+				result[stylesheet][selector] = {};
+				for(let prop in styles[selector]) {
 					result[stylesheet][selector][prop] = styles[selector][prop];
 				}
 			}
 		}
 	}
 	return result;
-}
+};
 
 // combining selectors
-var combineSelectors = function(rules, preventCombining) {
+var combineSelectors = function(rules, prevent_combining) {
 
 	var map = [], arr = {};
-	var preventCombining = [].concat(preventCombining || []);
+	var preventCombining = [].concat(prevent_combining || []);
 	preventCombining.splice(0, 0, '');
 	preventCombining = preventCombining.join('|');
 
 	// extracting every property
-	for(var selector in rules) {
+	for(let selector in rules) {
 		var props = rules[selector];
-		for(var prop in props) {
+		for(let prop in props) {
 			map.push({
 				selector: selector, 
 				prop: prop, 
@@ -88,9 +87,9 @@ var combineSelectors = function(rules, preventCombining) {
 	}
 
 	// combining
-	for(var i=0; i<map.length; i++) {
+	for(let i=0; i<map.length; i++) {
 		if(map[i].combine === true && map[i].selector !== false) {
-			for(var j=i+1;j<map.length; j++) {
+			for(let j=i+1;j<map.length; j++) {
 				if(map[i].prop === map[j].prop && map[i].value === map[j].value) {
 					map[i].selector += ', ' + map[j].selector.replace(/~~(.+)~~/, '').replace(/^%.*?%/, '');
 					map[j].selector = false; // marking for removal
@@ -100,15 +99,15 @@ var combineSelectors = function(rules, preventCombining) {
 	}
 
 	// preparing the result
-	for(var i=0; i<map.length; i++) {
+	for(let i=0; i<map.length; i++) {
 		if(map[i].selector !== false) {
-			if(!arr[map[i].selector]) arr[map[i].selector] = {}
+			if(!arr[map[i].selector]) { arr[map[i].selector] = {}; }
 			arr[map[i].selector][map[i].prop] = map[i].value;
 		}
 	}
 	
 	return arr;
-}
+};
 
 var minimize = function(content) {
     content = content.replace( /\/\*(?:(?!\*\/)[\s\S])*\*\/|[\r\n\t]+/g, '' );
@@ -120,14 +119,14 @@ var minimize = function(content) {
     content = content.replace( /([;,]) /g, '$1' );
     content = content.replace( / !/g, '!' );
     return content;
-}
+};
 
 var replaceDefined = function(css, options) {
 	if(options && options.api && options.api.getStorage().__defined) {
 		var storage = options.api.getStorage().__defined;
-		for(var prop in storage) {
+		for(let prop in storage) {
 			var re = new RegExp('<%( )?' + prop + '( )?%>', 'g');
-			if(typeof storage[prop] != 'function') {
+			if(typeof storage[prop] !== 'function') {
 				css = css.replace(re, storage[prop]);
 			} else {
 				css = css.replace(re, storage[prop]());
@@ -135,7 +134,7 @@ var replaceDefined = function(css, options) {
 		}
 	}
 	return css;
-}
+};
 
 export default function() {
 	var processor = function(rules, callback, options) {
@@ -146,7 +145,7 @@ export default function() {
 			return json;
 		}
 		var css = '';
-		for(var stylesheet in rules) {
+		for(let stylesheet in rules) {
 			var r = rules[stylesheet];
 			r = options.combineSelectors ? combineSelectors(r, options.preventCombining) : r;
 			if(stylesheet === "mainstream") {
@@ -163,12 +162,12 @@ export default function() {
 		// Minification
 		if(options.minify) {
 			css = minimize(css);
-			if(callback) callback(null, css);
+			if(callback) { callback(null, css); }
 		} else {
-			if(callback) callback(null, css);
+			if(callback) { callback(null, css); }
 		}
 		return css;
-	}
+	};
 	processor.type = "css";
 	return processor;
 }
